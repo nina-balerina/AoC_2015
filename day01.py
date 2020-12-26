b777d26123fb99169944e85cd9d9a0a99e12d287
Nina Ceh <71320289+nina-balerina@users.noreply.github.com>
2020-12-26 08:50:46 +0100

    Initial commit

---
+++ day01.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+@author: nina
+"""
+
+import time
+start_time = time.time()
+
+f = open('day01.txt', 'r')
+txt = f.read()
+
+floor = 0
+for char in txt:
+    if char == '(':
+        floor += 1
+    elif char == ')':
+        floor -= 1
+        
+        
+print("party 1: %s" % floor) #280
+
+floor = 0
+counter = 0
+for char in txt:
+    counter += 1
+    if char == '(':
+        floor += 1
+    elif char == ')':
+        floor -= 1
+    if floor < 0:
+        break
+    
+print("party 2: %s" % floor) #1797
