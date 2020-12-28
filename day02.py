dd05a6003753b6605f5f8c2857e144606deda7c6
Nina Ceh <71320289+nina-balerina@users.noreply.github.com>
2020-12-28 20:07:26 +0100

    Update in names
    for box in boxes_dimensions
    changed to:
    for (l,w,h) in boxes_dimensions

--- day02.py
+++ day02.py
@@ -9,25 +9,23 @@ start_time = time.time()
 f = open('day02.txt', 'r')
 txt = f.readlines()
 
-box_dimensions = []
+boxes_dimensions = []
 for line in txt:
     dimensions = line.strip().split('x')
-    dimensions_number = []
-    for dimension in dimensions:
-        dimensions_number.append(int(dimension))
-    box_dimensions.append(sorted(dimensions_number))
+    boxes_dimensions.append(sorted(int(n) for n in dimensions))
 
-total_paper_area = 0    
-for box in box_dimensions:
-    paper_per_box = 3*(box[0]*box[1]) + 2*(box[1]*box[2]) + 2*(box[2]*box[0])
+
+total_paper_area = 0  
+total_ribbon_length = 0  
+for (l,w,h) in boxes_dimensions: 
+    
+    paper_per_box = 3*(l*w) + 2*(w*h) + 2*(h*l)
     total_paper_area += paper_per_box
+    
+    ribbon_per_box = 2*(l+w) + l*w*h
+    total_ribbon_length += ribbon_per_box
+
 
 print(f"party 1: {total_paper_area}") #1598415
-
-total_ribbon_length = 0
-for box in box_dimensions:
-    ribbon_per_box = 2*(box[0]+box[1]) + box[0]*box[1]*box[2]
-    total_ribbon_length += ribbon_per_box
-    
 print(f"party 2: {total_ribbon_length}") #3812909
 print(f"---{time.time()-start_time} seconds---")
