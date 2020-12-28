7a6c5df4a3d00a4645f4e90bc4cd418cfaa6ac2a
Nina Ceh <71320289+nina-balerina@users.noreply.github.com>
2020-12-28 19:16:25 +0100

    A function to change the position in each step is defined as change_position

--- day03.py
+++ day03.py
@@ -9,53 +9,36 @@ start_time = time.time()
 f = open('day03.txt', 'r')
 txt = f.read()
 
-visited_addresses = {}
+def change_position(x,y,direction):
+    if character == '>': x += 1
+    elif character == '^': y += 1
+    elif character == '<': x -= 1
+    elif character == 'v': y -= 1
+    return[x,y]
+    
+
+visited_addresses = set()
 position = [0]*2    # x, y
 for character in txt:
-    if character == '>':
-        position[0] += 1
-    elif character == '^':
-        position[1] += 1
-    elif character == '<':
-        position[0] -= 1
-    elif character == 'v':
-        position[1] -= 1
+    [position[0], position[1]] = change_position(position[0], position[1], character)
     if (position[0],position[1]) not in visited_addresses:
-        visited_addresses[(position[0],position[1])] = 1
+        visited_addresses.add((position[0],position[1]))
 
 print(f"party 1: {len(visited_addresses)}") #2572
 
-visited_addresses_total = {}
+visited_addresses_total = set()
 position_Santa = [0]*2    # x, y
 position_RoboSanta = [0]*2
 counter = 0
 for character in txt:
     if counter%2 == 0:
-        if character == '>':
-            position_Santa[0] += 1
-        elif character == '^':
-            position_Santa[1] += 1
-        elif character == '<':
-            position_Santa[0] -= 1
-        elif character == 'v':
-            position_Santa[1] -= 1
-        p1 = position_Santa[0]
-        p2 = position_Santa[1]
-        
+        [position_Santa[0], position_Santa[1]] = change_position(position_Santa[0], position_Santa[1], character)
+        [p1, p2] = [position_Santa[0], position_Santa[1]]       
     elif counter%2 == 1:
-        if character == '>':
-            position_RoboSanta[0] += 1
-        elif character == '^':
-            position_RoboSanta[1] += 1
-        elif character == '<':
-            position_RoboSanta[0] -= 1
-        elif character == 'v':
-            position_RoboSanta[1] -= 1
-        p1 = position_RoboSanta[0]
-        p2 = position_RoboSanta[1]
-        
+        [position_RoboSanta[0], position_RoboSanta[1]] = change_position(position_RoboSanta[0], position_RoboSanta[1], character)
+        [p1, p2] = [position_RoboSanta[0], position_RoboSanta[1]]        
     if (p1,p2) not in visited_addresses_total:
-        visited_addresses_total[(p1,p2)] = 1
+        visited_addresses_total.add((p1,p2))
     counter += 1
     
 print(f"party 2: {len(visited_addresses_total)}") #2631
